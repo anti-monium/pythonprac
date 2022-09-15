2ee9e3ce035b76427bc7d37deb69a6e3ef17b8e4
Голубкова Мария <mrzzz13169@gmail.com>
2022-09-15 21:16:16 +0300

    Улучшила решение

--- 20220913/2/prog.py
+++ 20220913/2/prog.py
@@ -1,8 +1,2 @@
-a = input()
-if (len(a) <= 2):
-	quit()
-a = list(a[1:len(a) - 1].split(','))
-for i in range (0, len(a)):
-	a[i] = int(a[i])
-a = sorted(a)
-print(*a, sep = ", ", end = '\n')
+a = eval(input())
+print(*sorted(a), sep = ", ")
