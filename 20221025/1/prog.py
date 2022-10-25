95a6f3c784f55ee33a821c529fcd86c2ecca100e
Maria <mrzzz13169@gmail.com>
2022-10-25 21:26:32 +0300

    Это не много, но это честная работа

--- 20221025/1/prog.py
+++ 20221025/1/prog.py
@@ -1,1 +1,11 @@
-Я напишу обязательно, но я хочу кушать
+def fib(m,  n):
+	i = 0	
+	cur, next = 1, 1
+	while i <= n:
+		if i >= m:
+			yield cur
+		cur, next = next, cur + next
+		i += 1
+		
+import sys
+exec(sys.stdin.read())
