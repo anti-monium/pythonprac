3428360c156802c7db5dcf0486ef4d47d2808f85
Maria <mrzzz13169@gmail.com>
2022-10-31 22:06:43 +0300

    Усё

--- 20221025/2/prog.py
+++ 20221025/2/prog.py
@@ -3,19 +3,15 @@ import itertools as itt
 def slide(seq, n):
 	i = 0
 	while True:
-		#print(f"_{i}_{i+n}_", end = '')
-		yield from itt.islice(seq, n)
-		i += 1
+		seq, new = itt.tee(seq)
+		ans = itt.islice(new, i, n + i)
+		ch, ans = itt.tee(ans)
+		if len(list(ch)) > 0:
+			i += 1
+			yield from ans
+		else:
+			break
 		
-def fib():
-	cur, next = 1, 1
-	while True:
-		yield cur
-		cur, next = next, cur + next
 
-i = slide(fib(), 3)
-s = next(i)
-while s <= 22:
-	print(s, end=' ')
-	s = next(i)
-print()
+import sys
+exec(sys.stdin.read())
