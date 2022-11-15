b3f8a53f045affb1233bae799c9100cd48df835c
Maria <mrzzz13169@gmail.com>
2022-11-15 23:53:24 +0300

    smth new

---
+++ 20221115/1/prog.py
@@ -0,0 +1,11 @@
+def objcount(clss):
+	clss.counter = 0
+	def init(self, *args = None)
+		clss.counter += 1
+		try:
+			clss.__init__(self, *args)
+		except Exception
+	
+	def del
+	
+	clss.__init__ = init
