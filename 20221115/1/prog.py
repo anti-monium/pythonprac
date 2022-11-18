3b80211d9e80e710e970a26594e08b8b57e34637
Maria <mrzzz13169@gmail.com>
2022-11-18 17:14:32 +0300

    task1

--- 20221115/1/prog.py
+++ 20221115/1/prog.py
@@ -1,11 +1,34 @@
 def objcount(clss):
 	clss.counter = 0
-	def init(self, *args = None)
+	def i(*x):
+		pass
+	def d(x):
+		pass
+	if '__init__' in clss.__dict__:
+		i = clss.__init__
+	if '__del__' in clss.__dict__:
+		d = clss.__del__
+	def _init_(self, args = None):
 		clss.counter += 1
 		try:
-			clss.__init__(self, *args)
-		except Exception
+			if args == None:
+				i(self)
+			else:
+				i(self, *args)
+		except Exception:
+			pass
 	
-	def del
-	
-	clss.__init__ = init
+	def _del_(self):
+		clss.counter -= 1
+		try:
+			d(self)
+		except Exception:
+			pass
+			
+	clss.__init__ = _init_
+	clss.__del__ = _del_
+			
+	return clss
+			
+import sys
+exec(sys.stdin.read())
