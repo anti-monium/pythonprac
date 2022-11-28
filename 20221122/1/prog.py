cef40c53087ad673c7ecabf90b36c11d755dc396
Maria <mrzzz13169@gmail.com>
2022-11-28 23:08:38 +0300

    task1

--- 20221122/1/prog.py
+++ 20221122/1/prog.py
@@ -1,1 +1,12 @@
-Я сделаю домашку но потом, точно не сейчас((((((
+import sys
+
+s = sys.stdin.buffer.read()
+N = s[0]
+sys.stdout.buffer.write(s[0:1])
+s = s[1:]
+l = len(s)
+s_new = []
+for i in range(N):
+	s_new.append(s[i * l // N:(1 + i) * l // N])
+for b in sorted(s_new):
+	sys.stdout.buffer.write(b)
