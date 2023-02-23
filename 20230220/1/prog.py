51d1d4885677b10b412c16b13fe6ea0a9f5e8458
Maria <mrzzz13169@gmail.com>
2023-02-23 23:25:12 +0300

    Простенькая бродилка

---
+++ 20230220/1/prog.py
@@ -0,0 +1,52 @@
+import cowsay
+
+n = 10
+field = [[0 for i in range(n)] for j in range(n)]
+
+class Gamer:
+    x = 0
+    y = 0
+    
+    def move_to(self, s):
+        if s == 'up':
+            self.x = (self.x - 1) % n
+        elif s == 'down':
+            self.x = (self.x + 1) % n
+        elif s == 'left':
+            self.y = (self.y - 1) % n
+        elif s == 'right':
+            self.y = (self.y + 1) % n
+
+  
+class Monster:
+    def __init__(self, x, y, s):
+        self.hello = s
+        
+
+def encounter(x, y):
+    print(cowsay.cowsay(field[y][x].hello))
+
+
+g = Gamer()
+while True:
+    s = input().split()
+    match s:
+        case [('up' | 'down' | 'left' | 'right')]:
+            g.move_to(s[0])
+            print(f"Moved to ({g.x}, {g.y})")
+            if field[g.y][g.x]:
+                encounter(g.x, g.y)
+        case ['addmon', *opt]:
+            try:
+                x, y, hello = int(opt[0]), int(opt[1]), opt[2]
+                _ = field[y][x]
+            except:
+                print('Invalid arguments')
+                continue
+            print(f"Added monster to ({x}, {y}) saying {hello}")
+            if field[y][x]:
+                print('Replaced the old monster')
+                continue
+            field[y][x] = Monster(x, y, hello)
+        case _:
+            print('Invalid command')
